75c86e002f0c1fde62b28f4d6fb4b6f924371537
kainomad <33932198+kainomad@users.noreply.github.com>
2019-04-02 16:58:51 +0200

    Add files via upload

---
+++ Vecteur.h
@@ -0,0 +1,21 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : 04
+ Fichier     : main.cpp
+ Groupe      : Labo_04
+ Auteur(s)   : Chau Ying Kot, Johann Werkle
+ Date        : 02.04.2019
+ 
+ But         :
+ 
+ Remarque(s) : -
+ 
+ Compilateur : MinGW-g++ 6.3.0
+ -----------------------------------------------------------------------------------
+ */
+
+#ifndef Vecteur_h
+#define Vecteur_h
+
+
+#endif /* Vecteur_h */
