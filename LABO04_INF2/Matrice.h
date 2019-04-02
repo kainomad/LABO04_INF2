ffde8698e6c9e08de15404eddac9dbe1a9b9f1ef
kainomad <33932198+kainomad@users.noreply.github.com>
2019-04-02 16:56:01 +0200

    Add files via upload

---
+++ LABO04_INF2/Matrice.h
@@ -0,0 +1,20 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : 04
+ Fichier     : main.cpp
+ Groupe      : Labo_04
+ Auteur(s)   : Chau Ying Kot, Johann Werkle
+ Date        : 02.04.2019
+ 
+ But         :
+ 
+ Remarque(s) : -
+ 
+ Compilateur : MinGW-g++ 6.3.0
+ -----------------------------------------------------------------------------------
+ */
+#ifndef Matrice_h
+#define Matrice_h
+
+
+#endif /* Matrice_h */
