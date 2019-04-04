d7174eed2bdd7f6bb7cba7045ba16e55d514dc98
KurohanaJuri <Chau-ying-kot@hotmail.com>
2019-04-04 21:45:04 +0200

    Add Matrice constructor

--- Matrice.h
+++ Matrice.h
@@ -16,5 +16,47 @@
 #ifndef Matrice_h
 #define Matrice_h
 
+#include <Vecteur.h>
+
+template <typename T> using Ligne = Vecteur<T>;
+
+
+template <typename T>
+class Matrice;
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const Matrice<T>& matrice){
+    os << "[";
+    for(size_t i = 0; i < matrice.size(); ++i){
+        os << matrice.at(i);
+    }
+    os << "]";
+    return os ;
+}
+
+template <typename T>
+class Matrice{
+    friend std::ostream& operator<< <T> (std::ostream& os, const Matrice<T>& vecteur);
+
+
+public:
+    Matrice() {}
+    Matrice(size_t ligne) : matrice(Vecteur<Vecteur<T>>(ligne)) {};
+    Matrice(const Vecteur<T>& ligne, const Vecteur<T>& colonne) {
+        for(size_t i = 0; i < ligne ;++i){
+            matrice.at(i) = Ligne<T>(colonne);
+        }
+    };
+
+    size_t size() const noexcept ;
+
+    Ligne<T>& at(size_t i);
+    const Ligne<T>& at(size_t i) const;
+
+
+private:
+    Vecteur<Ligne<T>> matrice;
+};
+
 #include "MatriceImpl.h"
 #endif /* Matrice_h */
