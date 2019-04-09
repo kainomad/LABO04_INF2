688f362a37777515080b6af5b126b5c75b2fb2d7
KurohanaJuri <Chau-ying-kot@hotmail.com>
2019-04-09 17:00:34 +0200

    oups

--- Vector_errors.h
+++ Vector_errors.h
@@ -2,22 +2,28 @@
 // Created by chauying on 04.04.19.
 //
 
-#include <stdexcept>
 
 #ifndef LABO04_INF2_VECTOR_ERROR_H
 #define LABO04_INF2_VECTOR_ERRORS_H
 
+#include <stdexcept>
 
-class OutOfRange : public std::logic_error{
+
+class DepacementCapacite : public std::out_of_range {
 public:
-    OutOfRange(const std::string& what_arg) noexcept : logic_error(what_arg){}
-    OutOfRange(const char* what_arg) noexcept : logic_error(what_arg) {}
+    DepacementCapacite(const std::string& classe,
+                       const std::string& what_arg = ": le resultat de l'operation depasse la capcite du type") noexcept
+            : out_of_range(what_arg), currentClass(classe) {}
+
+    DepacementCapacite(const std::string& classe, const char* what_arg) noexcept
+            : out_of_range(what_arg), currentClass(classe) {}
+
     std::string where() const noexcept {
         return currentClass;
     }
 
 private:
-    const std::string currentClass = "Vecteur";
+    const std::string currentClass;
 };
 
 
